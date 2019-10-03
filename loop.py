e53f5d59b0c8f0cf49b60b2cbc631366e69cad38
Zaida Rivai <zaida.rivai@gmail.com>
2019-10-03 11:06:50 +0200

    changes

--- loop.py
+++
@@ -1,8 +0,0 @@
-filename = "labels.txt"
-
-with open(filename) as f:
-    content = f.readlines()
-# you may also want to remove whitespace characters like `\n` at the end of each line
-content = [x.strip() for x in content]
-
-f.close()
